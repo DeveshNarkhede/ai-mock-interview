6e84b86f915e79c9f26b6378ccdb57d22a528f62
Devesh Narkhede <deveshnarkhede34@gmail.com>
2025-05-14 23:30:12 +0530

    Create authentication handler and complete React frontend

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ import { SignInPage } from "./routes/sign-in";
 import { SignUpPage } from "./routes/sign-up";
 import ProtectRoutes from "./layouts/protected-routes";
 import { MainLayout } from "./layouts/main-layout";
+import { MockInterviewPage } from "./routes/mock-interview-page";
+import { Feedback } from "./routes/feedback";
+import { MockLoadPage } from "./routes/mock-load-page";
+import { CreateEditPage } from "./routes/create-edit-page";
+import { Dashboard } from "./routes/dashboard";
+import { Generate } from "./components/generate";
 
 const App = () => {
   return (
@@ -30,11 +36,21 @@ const App = () => {
             </ProtectRoutes>
           }
         >
-
+          {/* add all the protect routes */}
+          <Route element={<Generate />} path="/generate">
+            <Route index element={<Dashboard />} />
+            <Route path=":interviewId" element={<CreateEditPage />} />
+            <Route path="interview/:interviewId" element={<MockLoadPage />} />
+            <Route
+              path="interview/:interviewId/start"
+              element={<MockInterviewPage />}
+            />
+            <Route path="feedback/:interviewId" element={<Feedback />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
-  )
+  );
 };
 
 export default App;
