867f7cfacf70706fe6edb5199e60d09006e76391
Devesh Narkhede <deveshnarkhede34@gmail.com>
2025-06-19 20:36:15 +0530

    Updated clerk authentication sign in / sign up

---
+++ src/layouts/main-layout.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/footer"
+import Header from "@/components/header"
+
+export const MainLayout = () => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+
+      
+
+      <Footer />
+    </div>
+  )
+}
